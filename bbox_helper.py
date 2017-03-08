34d4c475be809913afcdd3b353f5e18c765ab4b1
andrea_palazzi <ndrplz@gmail.com>
2017-03-08 16:35:22 +0100

    Added label to class Rectangle
    Added possibility to initialize a Rectangle with a certain label.
    In this case, when the Rectangle is drawn, an additional parameter
    draw_label in the draw function allows to draw the label on the top of
    the Rectangle.

--- bbox_helper.py
+++ bbox_helper.py
@@ -18,7 +18,7 @@ class Rectangle:
         y coordinate of bottom-right corner.
     """
     
-    def __init__(self, x_min, y_min, x_max, y_max):
+    def __init__(self, x_min, y_min, x_max, y_max, label=""):
 
         self.x_min = x_min
         self.y_min = y_min
@@ -27,6 +27,8 @@ class Rectangle:
 
         self.x_side = self.x_max - self.x_min
         self.y_side = self.y_max - self.y_min
+
+        self.label=""
 
     def intersect_with(self, rect):
         """
@@ -95,7 +97,7 @@ class Rectangle:
 
         return Rectangle(new_x_min, new_y_min, new_x_max, new_y_max)
 
-    def draw(self, frame, color=255, thickness=1):
+    def draw(self, frame, color=255, thickness=1, draw_label=False):
         """
         Draw Rectangle on a given frame.
 
@@ -109,11 +111,34 @@ class Rectangle:
             Color used to draw the rectangle (default = 255)
         thickness : int, optional
             Line thickness used t draw the rectangle (default = 1)
+        draw_label : bool, optional
+            If True and the Rectangle has a label, draws it on the top of the rectangle.
 
         Returns
         -------
         None
         """
+        if draw_label and self.label:
+
+            # compute text size
+            text_font, text_scale, text_thick = cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1
+            (text_w, text_h), baseline = cv2.getTextSize(self.label, text_font, text_scale, text_thick)
+
+            # draw rectangle on which text will be displayed
+            text_rect_w = min(text_w, self.x_side - 2 * baseline)
+            out = cv2.rectangle(frame.copy(), pt1=(self.x_min, self.y_min - text_h - 2 * baseline),
+                          pt2=(self.x_min + text_rect_w + 2 * baseline, self.y_min), color=color, thickness=cv2.FILLED)
+            cv2.addWeighted(frame, 0.75, out, 0.25, 0, dst=frame)
+
+            # actually write text label
+            cv2.putText(frame, self.label, (self.x_min + baseline, self.y_min - baseline),
+                        text_font, text_scale, (0, 0, 0), text_thick, cv2.LINE_AA)
+
+            # add text rectangle border
+            cv2.rectangle(frame, pt1=(self.x_min, self.y_min - text_h - 2 * baseline),
+                          pt2=(self.x_min + text_rect_w + 2 * baseline, self.y_min), color=color, thickness=thickness)
+
+        # draw the Rectangle
         cv2.rectangle(frame, (self.x_min, self.y_min), (self.x_max, self.y_max), color, thickness)
 
     def get_binary_mask(self, mask_shape):
@@ -179,3 +204,14 @@ class Rectangle:
         area : float32
         """
         return np.float32(self.x_side * self.y_side)
+
+
+if __name__ == '__main__':
+
+    img = cv2.imread('img/test1.jpg')
+    r = Rectangle(100, 100, 300, 300)
+    r.label = "hello world!"
+    r.draw(img, draw_label=True)
+
+    cv2.imshow('', img)
+    cv2.waitKey()
